6dfe284b3ab0a387cddeee70bf6d5595949f8bf7
独行 <jsw0325@163.com>
2022-03-04 23:12:46 +0800

    feat: 搭建 layout 布局页面

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const item = {
+  date: '2016-05-02',
+  name: 'Tom',
+  address: 'No. 189, Grove St, Los Angeles',
+}
+const tableData = ref(Array(30).fill(item))
+</script>
+
+<template>
+  <el-main>
+    <el-scrollbar>
+      <el-table :data="tableData">
+        <el-table-column prop="date" label="Date" width="140">
+        </el-table-column>
+        <el-table-column prop="name" label="Name" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="Address"> </el-table-column>
+      </el-table>
+    </el-scrollbar>
+  </el-main>
+</template>
---
+++ src/layout/page-layout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Message, Menu as IconMenu, Setting } from '@element-plus/icons-vue'
+import HeaderBar from './components/HeaderBar.vue'
+import SideBar from './components/SideBar.vue'
+import AppMain from './components/AppMain.vue'
+
+const isHeaderFixed = ref(false)
+</script>
+
+<template>
+  <el-container class="layout-container">
+    <SideBar />
+
+    <el-container direction="vertical">
+      <el-scrollbar v-if="isHeaderFixed" style="width: 100%">
+        <HeaderBar />
+        <AppMain />
+      </el-scrollbar>
+
+      <template v-else>
+        <HeaderBar />
+        <AppMain />
+      </template>
+    </el-container>
+  </el-container>
+</template>
+
+<style lang="scss" scoped></style>
